27dca51eacfcea329ee04c819752393c43f5a8ac
Joshua Rudio <joshuarudio@gmail.com>
2018-12-20 09:16:39 -0800

    Add code for day 1

---
+++ day1.rb
@@ -0,0 +1,20 @@
+inputs = []
+counter = 0
+
+file = File.open( 'input1.txt' )
+file.each_line do | line |
+  inputs.push( line )
+end
+
+inputs.each do | input |
+  case input[ 0 ]
+  when '+'
+    number = input.gsub( '+', '' )
+    counter += number.to_i
+  when '-'
+    number = input.gsub( '-', '' )
+    counter -= number.to_i
+  end
+end
+
+puts counter
